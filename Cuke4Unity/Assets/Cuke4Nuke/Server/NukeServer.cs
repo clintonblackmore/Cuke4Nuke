3c898e6e124a76c6ce0019f8e9a6b5f9eb2f3072
Clinton Blackmore <clinton.blackmore@gmail.com>
2016-11-23 20:32:07 -0700

    Wire Server reloads code when Unity does a live-recompile

--- Cuke4Unity/Assets/Cuke4Nuke/Server/NukeServer.cs
+++ Cuke4Unity/Assets/Cuke4Nuke/Server/NukeServer.cs
@@ -11,17 +11,29 @@ namespace Cuke4Nuke.Server
         //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         private readonly Listener _listener;
-        readonly Options _options;
 
-        public NukeServer(Listener listener, Options options)
+        public NukeServer(Listener listener)
         {
             _listener = listener;
-            _options = options;
         }
 
         public void Start()
         {
             Run();
+        }
+
+        public void Stop()
+        {
+            _listener.MessageLogged -= listener_LogMessage;
+            try
+            {
+                _listener.Stop();
+            }
+            catch (Exception e)
+            {
+                string message = "Unable to gracefully stop listener. Exception:\n\n" + e.Message;
+                Debug.LogError(message);
+            }
         }
 
         void Run()
@@ -36,29 +48,6 @@ namespace Cuke4Nuke.Server
                 string message = "Unable to start listener. Exception:\n\n" + ex.Message;
                 Debug.LogError(message);
             }
-            /*
-            finally
-            {
-                Debug.Log("Finally");
-                try
-                {
-                    _listener.Stop();
-                }
-                catch (Exception e)
-                {
-                    string message = "Unable to gracefully stop listener. Exception:\n\n" + e.Message;
-                    Debug.LogError(message);
-                }
-            }
-            */
-        }
-
-        void ShowHelp()
-        {
-            Debug.Log("Usage: Cuke4Nuke.Server.exe [OPTIONS]");
-            Debug.Log("Start the Cuke4Nuke server to invoke .NET Cucumber step definitions.");
-            Debug.Log("");
-            Debug.Log(_options.ToString());
         }
 
         void listener_LogMessage(object sender, LogEventArgs e)
--- Cuke4Unity/Assets/Cuke4Nuke/Server/NukeServerComponent.cs
+++ Cuke4Unity/Assets/Cuke4Nuke/Server/NukeServerComponent.cs
@@ -13,22 +13,49 @@ namespace Cuke4Nuke.Server
    {
         public Cuke4Nuke.Server.Options options;
         public Text display;
+        NukeServer server;
+        int timesLoaded = 0;
 
     	// Use this for initialization
-    	void Start () 
+    	void OnEnable() 
         {
+            Debug.Log("OnEnable called");
+
             var objectFactory = new ObjectFactory();
             var loader = new Loader(objectFactory);
             var processor = new Processor(loader, objectFactory);
             var listener = new AsyncListener(processor, options.Port);
  
-            new NukeServer(listener, options).Start();
+            server = new NukeServer(listener);
+            server.Start();
 
             if (display != null)
             {
-                display.text = string.Format("Listening on port {0} at \n{1}", options.Port, GetIPAddresses());
+                string timesCompiledMessage = "";
+                if (timesLoaded == 1)
+                {
+                    timesCompiledMessage = string.Format("Recompiled once");
+                } 
+                else if (timesLoaded > 1)
+                {
+                    timesCompiledMessage = string.Format("Recompiled {0} times", timesLoaded);
+                }
+
+                display.text = string.Format("Listening on port {0} at \n{1}\n\n{2}", options.Port, GetIPAddresses(), timesCompiledMessage);
             }
+
+            timesLoaded++;
     	}
+
+        void OnDisable()
+        {
+            Debug.Log("OnDisable called");
+            server.Stop();
+            if (display != null)
+            {
+                display.text = string.Format("Server stopped.");
+            }
+        }
 
         private string GetIPAddresses()
         {
